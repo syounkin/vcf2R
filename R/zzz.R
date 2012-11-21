218ae1fd0ceb2fc9971cf85c42feef16d8df76e1
Samuel Younkin <syounkin@jhsph.edu>
2012-11-21 12:20:45 -0500

    Updated zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,17 @@
 THISPKG <- "vcf2R"
 .onAttach <- function(libname, pkgname) {
 	version <- packageDescription("vcf2R", field="Version")
-	packageStartupMessage(paste("=================================
-Welcome to vcf2R version ", version, "\nby Samuel G. Younkin\nThe package for parsing vcftools output files and creating nice R objects.\nI hope that you will be loving it!\n=================================", sep = "" ) )
+	packageStartupMessage(paste("
+==================================
+Welcome to vcf2R version ", version, "\n",
+"by Samuel G. Younkin
+
+The package for parsing vcftools
+output files and creating nice R
+objects.
+
+I hope that you will be loving it!
+==================================", sep = "" ) )
 }
 
 .onUnload <- function(libpath){
