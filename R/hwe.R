9509bd03b308b3f2851b43bf16212ad69724ecd4
Samuel Younkin <syounkin@jhsph.edu>
2012-11-21 13:00:34 -0500

    Furthered the parsing done by hwe2R

--- R/hwe.R
+++ R/hwe.R
@@ -1,5 +1,19 @@
 hwe2R <- function( file ){
-  hwe.df <- data.frame(scan( file = file, sep = "\t", what = list( "", 1, "", "", 1, 1 ), skip = 1 ))
-  names(hwe.df) <- c("CHR",	"POS",	"OBS(HOM1/HET/HOM2)",	"E(HOM1/HET/HOM2)",	"ChiSq",	"P" )
+  hwe <- data.frame(scan( file = file, sep = "\t", what = list( "", 1, "", "", 1, 1 ), skip = 1 ), stringsAsFactors = FALSE)
+  names(hwe) <- c("CHR",	"POS",	"OBS",	"EXP",	"ChiSq",	"P" )
+
+  hwe.df <-   with(hwe, {
+    obs.mat <- t(data.frame(strsplit(OBS, split = "/")))
+    data.frame(
+      chr = as.factor(CHR),
+      pos = as.integer(POS),
+      chi.sq = as.numeric(ChiSq),
+      p.value = as.numeric(P),
+      homo.major = as.integer(obs.mat[,1]),
+      het = as.integer(obs.mat[,2]),
+      homo.minor = as.integer(obs.mat[,3])
+      )
+  })
+  rownames(hwe.df) <- NULL
   return( hwe.df )
 }
