03b494af4fce3f14af3aac32fcf0f18750958ddc
Samuel Younkin <syounkin@jhsph.edu>
2012-11-28 15:21:05 -0500

    Updated demo file

--- demo/vcf2R.R
+++ demo/vcf2R.R
@@ -2,4 +2,7 @@ file <- system.file( "extdata", "snvs.fixed_header.frq", package = "vcf2R")
 maf.di <- frq2R( file )
 summary(maf.di)
 pos.df <- frq2pos( file )
-with( pos.df, table(chr) )
+head(pos.df)
+
+data("targets")
+
